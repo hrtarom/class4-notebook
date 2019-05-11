364083fe4d6e684489bc9178281b54583fb176c2
hrtarom <hamidreza.taremian@gmail.com>
2019-05-11 00:10:32 -0400

    first commit

--- my_csv_reader_new.py
+++ my_csv_reader_new.py
@@ -1,9 +1,9 @@
 
 import numpy as np
 import pandas as pd
-import argparse
+import argparse,sys
 
-data= pd.read_csv('breast_cancer.csv')
+data= pd.read_csv(sys.argv[1])
 Data=pd.DataFrame(data)
 for col  in range(len(Data.columns)):
     print ("The mean of the",Data.columns[col],"is",Data.iloc[:,col].mean())
